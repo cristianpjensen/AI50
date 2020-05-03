a3f6c6e814b1e7655684c7f9e9707070631ecb10
cristianpjensen <cristianpjensen@gmail.com>
2020-05-03 18:45:32 +0200

    Completed

--- 5. Neural Networks/traffic/traffic.py
+++ 5. Neural Networks/traffic/traffic.py
@@ -58,7 +58,21 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
-    raise NotImplementedError
+    images = []
+    labels = []
+    
+    # Append the - resized - numpy ndarrays of the images to the `images` 
+    # list, and append the corresponding category to the `labels` list.
+    for category in range(NUM_CATEGORIES):
+        data = os.listdir(os.path.join(data_dir, str(category)))
+        for image in data:
+            img = cv2.imread(os.path.join(data_dir, str(category), image), -1)
+            img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+
+            images.append(img)
+            labels.append(category)
+
+    return (images, labels)
 
 
 def get_model():
@@ -67,7 +81,26 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+    # The model (adjustable).
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu', 
+                               input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(NUM_CATEGORIES)
+    ])
+
+    # Compile the model.
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    return model
 
 
 if __name__ == "__main__":
